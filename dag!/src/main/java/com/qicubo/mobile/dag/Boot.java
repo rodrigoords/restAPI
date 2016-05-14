95560ad05e8c38fecefdf0c952ce5e705234871f
dagdev <dev@dag.hostzi.com>
2016-05-14 13:35:12 -0300

    Creating the project files

---
+++ dag!/src/main/java/com/qicubo/mobile/dag/Boot.java
@@ -0,0 +1,25 @@
+package com.qicubo.mobile.dag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+
+   @RequestMapping("/")
+   @ResponseBody
+   public String home()
+   {
+      return "home";
+   }
+}
