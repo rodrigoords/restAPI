45d90ac39de2f22edc4f12b86708204f959df5df
Rodrigo Sene <rodrigoords@hotmail.com>
2016-06-13 08:40:52 -0300

    correção de API para bolhas proximas

--- Back-end/dag/src/main/java/com/qicubo/mobile/dag/controllers/BolhaController.java
+++ Back-end/dag/src/main/java/com/qicubo/mobile/dag/controllers/BolhaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -26,96 +27,95 @@ import com.qicubo.mobile.dag.types.Longitude;
 @RestController
 public class BolhaController {
 
-	@Autowired
-	private BolhaService bolhaService;
-	@Autowired
-	private UsuarioService usuarioService;
-	@Autowired
-	private TipoService tipoService; 
+    @Autowired
+    private BolhaService bolhaService;
+    @Autowired
+    private UsuarioService usuarioService;
+    @Autowired
+    private TipoService tipoService;
 
-	
-	@RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_ALL_BOLHAS)
-	public ResponseEntity<List<Bolha>> list() {
+    @RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_ALL_BOLHAS)
+    public ResponseEntity<List<Bolha>> list() {
 
-		List<Bolha> bolhas = bolhaService.findAll();
+        List<Bolha> bolhas = bolhaService.findAll();
 
-		if (bolhas.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
+        if (bolhas.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-		return new ResponseEntity<>(bolhas, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_BY_ID)
-	public ResponseEntity<Bolha> getBolhaById(@PathVariable("id") Long id) {
-		Bolha bolha = bolhaService.findById(id);
-		if (bolha == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<>(bolha, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_IN_RANGE)
-	public ResponseEntity<List<Bolha>> getCloserBolhas(@PathVariable("lat, longi, index") Latitude lat, Longitude longi){
-		
-		List<Bolha> bolhas = bolhaService.findAllCloserBolhas(lat, longi);
-		
-		if (bolhas.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-		
-		return new ResponseEntity<>(bolhas, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(bolhas, HttpStatus.OK);
+    }
 
+    @RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_BY_ID)
+    public ResponseEntity<Bolha> getBolhaById(@PathVariable("id") Long id) {
+        Bolha bolha = bolhaService.findById(id);
+        if (bolha == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(bolha, HttpStatus.OK);
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_BY_USER_LOGIN)
-	public ResponseEntity<List<Bolha>> getBolhaByUser(@PathVariable("login") String login) {
+    @RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_IN_RANGE)
+    public ResponseEntity<List<Bolha>> getCloserBolhas(@RequestParam(value = "latitude", required = true) Latitude lat,
+            @RequestParam(value = "longitude", required = true) Longitude longi) {
 
-		Usuario user = usuarioService.findByLogin(login);
-		
-		if (user == null){
-		    return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
-		}
-		
-		List<Bolha> bolhas = bolhaService.findBolhaByUser(user);
-		if (bolhas.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<>(bolhas, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = BolhaRestURIConstants.CREATE_BOLHA)
-	public ResponseEntity<Void> createBolha(@RequestBody Bolha bolha, UriComponentsBuilder ucBuilder ){
-		
-		Tipo tipo = tipoService.findByName(bolha.getTipo().getNome());
-		
-		if (tipo == null){
-			return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
-		}
-		
-		Usuario usuario = usuarioService.findByLogin(bolha.getUsuarioCriacao().getLogin());
-		
-		if (usuario == null){
-			return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
-		}
-		
-		bolha.setUsuarioCriacao(usuario);
-		bolha.setTipo(tipo);
-		
-		if (bolhaService.isBolhaExist(bolha)){
-			return new ResponseEntity<>(HttpStatus.CONFLICT);
-		}
-		
-		Index indice = new Index(bolha.getLatitude().bigDecimalValue().subtract(bolha.getLongitude().bigDecimalValue()).toString());
-		
-		bolha.setIndice(indice);
-		
-		bolhaService.create(bolha);
-		
-		HttpHeaders headers = new HttpHeaders();
+        List<Bolha> bolhas = bolhaService.findAllCloserBolhas(lat, longi);
+
+        if (bolhas.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(bolhas, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = BolhaRestURIConstants.GET_BOLHA_BY_USER_LOGIN)
+    public ResponseEntity<List<Bolha>> getBolhaByUser(@PathVariable("login") String login) {
+
+        Usuario user = usuarioService.findByLogin(login);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
+        }
+
+        List<Bolha> bolhas = bolhaService.findBolhaByUser(user);
+        if (bolhas.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(bolhas, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = BolhaRestURIConstants.CREATE_BOLHA)
+    public ResponseEntity<Void> createBolha(@RequestBody Bolha bolha, UriComponentsBuilder ucBuilder) {
+
+        Tipo tipo = tipoService.findByName(bolha.getTipo().getNome());
+
+        if (tipo == null) {
+            return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
+        }
+
+        Usuario usuario = usuarioService.findByLogin(bolha.getUsuarioCriacao().getLogin());
+
+        if (usuario == null) {
+            return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
+        }
+
+        bolha.setUsuarioCriacao(usuario);
+        bolha.setTipo(tipo);
+
+        if (bolhaService.isBolhaExist(bolha)) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Index indice = new Index(
+                bolha.getLatitude().bigDecimalValue().subtract(bolha.getLongitude().bigDecimalValue()).toString());
+
+        bolha.setIndice(indice);
+
+        bolhaService.create(bolha);
+
+        HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ucBuilder.path(BolhaRestURIConstants.CREATE_BOLHA).buildAndExpand(bolha.getId()).toUri());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
-	}
-	
+    }
 
 }
