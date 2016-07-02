e32167bb9bea0224cdc27cff786aec153447818c
Rodrigo Sene <rodrigoords@hotmail.com>
2016-07-01 23:32:08 -0300

    Melhoria de diretorios

---
+++ Front-end/www/js/controllers/ChatCtrl.js
@@ -0,0 +1,14 @@
+/**
+ * Arquivo de Controle do Chat
+ *  
+ * @file Controllers/ChatCtrl.js 
+ * @authors André Queiroz, Rafael Mello e Rodrigo Sene
+ * @version 1.0
+ *
+ * @copyright 2016, Dag!
+ * 
+ */
+
+controllers.controller('ChatCtrl', function($scope, $stateParams, $q) {
+
+});
